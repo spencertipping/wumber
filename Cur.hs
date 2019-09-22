e4f80012d4b4b813eea652942935d3304e849afc
Spencer Tipping <spencer.tipping@gmail.com>
2019-09-22 09:16:00 -0600

    WIP using typeclass and return-type polymorphism, but now we have undecidable instances (womp womp)

--- Cur.hs
+++ Cur.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE IncoherentInstances #-}
@@ -29,10 +28,13 @@ x = X
 y = Y
 z = Z
 
-vec :: Num n => Qu d l n -> Coordinate -> V3 (Qu d l n)
-vec n X = V3 n zero zero
-vec n Y = V3 zero n zero
-vec n Z = V3 zero zero n
+fz :: Foo n => n
+fz = fromNum 0
+
+vec :: Foo n => n -> Coordinate -> V3 n
+vec n X = V3 n fz fz
+vec n Y = V3 fz n fz
+vec n Z = V3 fz fz n
 
 
 plus'  x y u = x u + y u
@@ -45,6 +47,7 @@ abs'    x u = abs (x u)
 signum' x u = signum (x u)
 
 
+type N n = Qu '[] 'DefaultLCSU n
 type Len = Qu '[ 'F Length One] 'DefaultLCSU Double
 
 km = Kilo :@ Meter
@@ -63,6 +66,37 @@ mg = Milli :@ Gram
 type P = V3 Double
 
 
+class Foo a where
+  fromNum :: Num b => b -> a
+
+instance Foo a => Foo (Coordinate -> a) where
+  fromNum x c = fromNum $ vec (fromNum x) c
+
+instance Fractional n => Foo (N n) where
+  fromNum x = fromNum x % (Meter :/ Meter)
+
+instance Fractional a => Foo (V3 a) where
+  fromNum x = V3 (fromNum x) (fromNum x) (fromNum x)
+
+instance (
+  Subset (CanonicalUnitsOfFactors (UnitFactorsOf unit))
+         (CanonicalUnitsOfFactors (LookupList dim 'DefaultLCSU)),
+  Subset (CanonicalUnitsOfFactors (LookupList dim 'DefaultLCSU))
+         (CanonicalUnitsOfFactors (UnitFactorsOf unit)),
+  dim ~ '[],
+  dim ~ DimFactorsOf (DimOfUnit unit),
+  UnitFactor (LookupList dim 'DefaultLCSU),
+  Unit unit, Foo a) => Foo (unit -> a) where
+
+  fromNum x u = fromNum $ fromNum x % u
+
+instance Foo a => Num a where
+  fromInteger x = fromNum (fromInteger x)
+
+instance Foo a => Fractional a where
+  fromRational x = fromNum (fromRational x)
+
+{-
 instance (
   Subset (CanonicalUnitsOfFactors (UnitFactorsOf unit))
          (CanonicalUnitsOfFactors (LookupList dim 'DefaultLCSU)),
@@ -84,7 +118,7 @@ instance (
   Fractional (unit -> Qu dim 'DefaultLCSU n) where
 
   fromRational x u = fromRational x % u
-
+-}
 
 instance Num a => Num (Coordinate -> V3 (Qu d l a))
 
