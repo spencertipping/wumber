c587600e0b6ffa010b30aaed5f621eb5a5ddbc5b
Spencer Tipping <spencer.tipping@gmail.com>
2019-10-12 02:41:31 -0600

    Using lenses to modify turtle state

--- Cur.hs
+++ Cur.hs
@@ -1,30 +1,33 @@
-{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE BlockArguments, TemplateHaskell #-}
 
 module Cur where
 
 import Control.Monad.Identity
 import Control.Monad.RWS.Strict
 import Graphics.Gloss
+import Lens.Micro
+import Lens.Micro.TH
 
 
-data TurtleState = TS !Point !Float
+data TurtleState = TS { _ts_loc   :: !Point,
+                        _ts_theta :: !Float }
 type CurM = RWST () Path TurtleState Identity
 
+makeLenses ''TurtleState
+
 runT :: CurM a -> Picture
-runT m = Line $ snd $ execRWS m () (TS (0, 0) 0)
+runT m = Line $ snd $ execRWS (do tell [(0, 0)]; m) () (TS (0, 0) 0)
 
 fd :: Float -> CurM ()
 fd d = do
   TS (x, y) t <- get
   let x' = x + d * cos t
       y' = y + d * sin t
-  tell [(x, y), (x', y')]
-  put $ TS (x', y') t
+  tell [(x', y')]
+  modify $ ts_loc .~ (x', y')
 
 rt :: Float -> CurM ()
-rt d = do
-  TS (x, y) t <- get
-  put $ TS (x, y) (t + d * pi / 180)
+rt d = modify $ ts_theta %~ (+ d * pi / 180)
 
 lt :: Float -> CurM ()
 lt = rt . negate
