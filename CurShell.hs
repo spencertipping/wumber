97f923d4112f90fca9b244151264db5f0fd04e8c
Spencer Tipping <spencer.tipping@gmail.com>
2019-10-14 08:47:15 -0600

    Aw yeah, we now have the basics of mouse interaction

--- CurShell.hs
+++ CurShell.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE NamedFieldPuns, LambdaCase, BlockArguments, TemplateHaskell #-}
+{-# OPTIONS_GHC -funbox-strict-fields #-}
+
 module Main where
 
 import Control.Concurrent
@@ -7,6 +9,7 @@ import Cur
 import qualified Data.ByteString.UTF8 as B8
 import Data.IORef
 import Data.Maybe
+import Debug.Trace
 import GHC.Float
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Interact as I
@@ -14,7 +17,8 @@ import Language.Haskell.Interpreter
 import Lens.Micro
 import Lens.Micro.TH
 import Linear.Matrix hiding (trace)
-import Linear.Metric (distance)
+import qualified Linear.Metric as LM
+import Linear.V2
 import Linear.V3
 import Linear.V4
 import Linear.Vector
@@ -30,10 +34,13 @@ data View = V { _vt     :: !(V3 Double),
                 _vp     :: !Bool,
                 _vrs    :: !(M33 Double),
                 _vm     :: !(M44 Double),
+                _vsz    :: !Double,
                 _vclipz :: !Double,
                 _vclipc :: !Color,
                 _vclipa :: !Float,
-                _vmouse :: (Modifiers, Maybe Point) }
+                _vhovd  :: !Float,
+                _vhovc  :: !Color,
+                _vmouse :: (Modifiers, Maybe MouseButton, Point) }
   deriving (Show)
 
 makeLenses ''View
@@ -42,10 +49,13 @@ init_view :: View
 init_view = V 0 0 0 1 True
               identity
               identity
+              1080
               maxBound
               (makeColor 0.8 0.8 0.9 1)
               0.05
-              (Modifiers Up Up Up, Nothing)
+              0.01
+              (makeColor 1.0 0.7 0.2 1)
+              (Modifiers Up Up Up, Nothing, (0, 0))
 
 rs_matrix :: View -> M33 Double
 rs_matrix v = V3 (V3 1    0   0)
@@ -90,19 +100,18 @@ compiler model f = do
   render model f
 
 
-xsz = 1080
-ysz = 1080
-xfd = float2Double . (/ xsz)
-yfd = float2Double . (/ ysz)
+f2d = float2Double
+d2f = double2Float
 
 screenify :: View -> [Element] -> Picture
-screenify v = scale xsz ysz . pictures . map (project v')
-  where v' = v & vm     .~ view_matrix v
+screenify v = scale sz sz . pictures . map (project v')
+  where sz = d2f (_vsz v)
+        v' = v & vm     .~ view_matrix v
                & vrs    .~ rs_matrix v
                & vclipc .~ withAlpha (_vclipa v) (_vclipc v)
 
 translate_rel :: V3 Double -> View -> View
-translate_rel d v = v & vt %~ (^+^ inv33 (rs_matrix v) !* d)
+translate_rel d v = v & vt %~ (^+^ inv33 (rs_matrix v) !* (d & _xy %~ (^/ _vsz v)))
 
 update_view :: Event -> View -> View
 
@@ -119,48 +128,62 @@ update_view (EventKey (Char c) Down _ _) = case c of
   'p' -> vp %~ not
   _   -> id
 
-update_view (EventKey (MouseButton LeftButton) Down m p) = vmouse .~ (m, Just p)
-update_view (EventKey (MouseButton LeftButton) Up   m p) = vmouse .~ (m, Nothing)
+update_view (EventKey (MouseButton b) d m@(Modifiers s c a) p)
+  | b == LeftButton || b == MiddleButton || b == RightButton
+    = if d == Down then vmouse .~ (m, Just b, p)
+                   else vmouse .~ (m, Nothing, p)
 
-update_view (EventKey (MouseButton b) Down (Modifiers s c Up) _)
-  | c == Up
-    = case (s, b) of
-        (Up,   WheelUp)   -> vz %~ (* 1.1)
-        (Up,   WheelDown) -> vz %~ (/ 1.1)
-        (Down, WheelUp)   -> translate_rel $ V3 0 0   0.01
-        (Down, WheelDown) -> translate_rel $ V3 0 0 (-0.01)
-        _                 -> id
+  | c == Up = case (s, b) of
+                (Up,   WheelUp)   -> vz %~ (* 1.1)
+                (Up,   WheelDown) -> vz %~ (/ 1.1)
+                (Down, WheelUp)   -> translate_rel $ V3 0 0   0.01
+                (Down, WheelDown) -> translate_rel $ V3 0 0 (-0.01)
+                _                 -> id
 
-  | c == Down
-    = case (s, b) of
-        (Up,   WheelUp)   -> vclipz %~ (* 1.1)
-        (Up,   WheelDown) -> vclipz %~ (/ 1.1)
-        (Down, WheelUp)   -> vclipa %~ (* 1.1)
-        (Down, WheelDown) -> vclipa %~ (/ 1.1)
-        _                 -> id
+  | c == Down = case (s, b) of
+                  (Up,   WheelUp)   -> vclipz %~ (* 1.1)
+                  (Up,   WheelDown) -> vclipz %~ (/ 1.1)
+                  (Down, WheelUp)   -> vclipa %~ (* 1.1)
+                  (Down, WheelDown) -> vclipa %~ (/ 1.1)
+                  _                 -> id
 
   | otherwise = id
 
-
-update_view (EventMotion (x, y)) = \v ->
+update_view (EventMotion p@(x, y)) = \v ->
   case _vmouse v of
-    (Modifiers Up Up Up, Just (x0, y0)) ->
-      translate_rel (V3 (xfd $ x - x0) (yfd $ y - y0) 0) v
-        & vmouse._2 .~ Just (x, y)
+    (Modifiers Up Up Up, Just LeftButton, (x0, y0)) ->
+      translate_rel (V3 (f2d $ x - x0) (f2d $ y - y0) 0) v
+        & vmouse._3 .~ p
 
-    (Modifiers Down Up Up, Just (x0, y0)) ->
-      v & vry %~ (+ xfd (x0 - x) * 360)
-        & vrx %~ (+ yfd (y0 - y) * 360)
-        & vmouse._2 .~ Just (x, y)
+    (Modifiers Down Up Up, Just LeftButton, (x0, y0)) ->
+      v & vry %~ (+ f2d (x0 - x) * 360 / _vsz v)
+        & vrx %~ (+ f2d (y0 - y) * 360 / _vsz v)
+        & vmouse._3 .~ p
 
-    _ -> v
+    _ -> v & vmouse._3 .~ p
 
 update_view _ = id
 
 
+{-# INLINE ldist #-}
+ldist :: (LM.Metric v, Floating a, Ord a) => v a -> v a -> v a -> a
+ldist l1 l2 v = LM.norm $ V2 dorth dpar
+  where l2'   = l2 ^-^ l1
+        v'    = v  ^-^ l1
+        ld    = LM.norm l2'
+        pvl1  = LM.project l2' v'
+        pvl2  = LM.project (l1 ^-^ l2) (v ^-^ l2)
+        dorth = LM.distance v' pvl1
+        dpar  = foldl1 max [0, LM.norm pvl1 - ld, LM.norm pvl2 - ld]
+
+
+{-# INLINE pv #-}
+pv :: (Float, Float) -> V2 Float
+pv (x, y) = V2 x y
+
+{-# INLINE pp #-}
 pp :: View -> V3 Double -> (Point, Double)
-pp v p = ((double2Float (x/w), double2Float (y/w)), z)
-  where V4 x y z w = _vm v !* point p
+pp v p = ((d2f (x/w), d2f (y/w)), z) where V4 x y z w = _vm v !* point p
 
 project :: View -> Element -> Picture
 project v (L3D c v1 v2)
@@ -169,9 +192,13 @@ project v (L3D c v1 v2)
 
   where (xy1, z1) = pp v v1
         (xy2, z2) = pp v v2
+        (_, _, m) = _vmouse v
+        d         = ldist (pv xy1) (pv xy2) (pv m ^/ d2f (_vsz v))
         clipz     = _vclipz v * _vz v
         c'        = if abs (z1-1) <= clipz && abs (z2-1) <= clipz
-                    then c
+                    then if d < _vhovd v
+                         then _vhovc v
+                         else c
                     else _vclipc v
 
 
