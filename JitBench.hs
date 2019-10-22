031d3c616c3768d6e7cc309878e312ce19d51109
Spencer Tipping <spencer.tipping@gmail.com>
2019-10-22 07:07:59 -0600

    Detailed benchmarking to find out what we need to optimize. Haskell does all right when everything is monomorphic and stream fusion kicks in -- but our use case defeats much of that.

--- JitBench.hs
+++ JitBench.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE BlockArguments #-}
 {-# OPTIONS_GHC -funbox-strict-fields #-}
 
 module Main where
 
 import Control.Monad
+import Debug.Trace
 import GHC.Float
 import Graphics.Gloss
 import Linear.Metric
@@ -17,46 +19,82 @@ import Text.Printf
 import Criterion.Main
 
 
-data Implicit = ISphere !Double !(V3 Double)
+-- NOTE: can't be Float; the model never converges
+type OurNumber = Double
+
+data Implicit = ISphere !OurNumber !(V3 OurNumber)
               | Intersect [Implicit]
               | Union     [Implicit]
               | Negate    Implicit
 
 for = flip map
 fi  = fromIntegral
-f2d = float2Double
 
 
--- WAT: this is 16x faster than eval???
-eval_sphere_handcoded (V3 x y z) (ISphere r (V3 cx cy cz)) =
+eval_sphere (ISphere r c) v = r - distance c v
+
+eval_sphere_handcoded (ISphere r (V3 cx cy cz)) (V3 x y z) =
   r - sqrt ( dx*dx + dy*dy + dz*dz )
   where dx = cx - x
         dy = cy - y
         dz = cz - z
 
 
-eval :: V3 Double -> Implicit -> Double
-eval v (ISphere r l)  = r - distance v l
-eval v (Intersect xs) = foldl1 min $ map (eval v) xs
-eval v (Union xs)     = foldl1 max $ map (eval v) xs
-eval v (Negate x)     = negate $ eval v x
+-- NOTE: eval is 16x slower than eval_handcoded without strictness annotations;
+-- with annotations, the two functions have the same performance within 5%.
+--
+-- Stream fusion doesn't happen between foldl1 and map. eval union with
+-- foldl1 max $ map ... is 40ns (100%) slower than the manually fused version.
+eval :: Implicit -> V3 OurNumber -> OurNumber
+eval (ISphere r !l) !v  = r - distance v l
+eval (Union !xs) !v     = foldl (\d x -> d `max` eval x v) (-1/0) xs
+--eval (Intersect !xs) !v = foldl1 min $ map (\m -> eval m v) xs
+--eval (Negate !x) !v     = negate $ eval x v
+
+
+eval_sphere_mono :: Implicit -> V3 OurNumber -> OurNumber
+--eval_sphere_mono = eval         -- 30% slower
+eval_sphere_mono = eval_sphere
+
+eval_union_mono :: Implicit -> V3 OurNumber -> OurNumber
+eval_union_mono (Union !xs) !v =
+  foldl (\d x -> d `max` eval_sphere_mono x v) (-1/0) xs
 
 
 ε = 1e-8
 
 -- | Returns the location and gradient of the surface intersection.
-solve :: V3 Double -> Implicit -> (V3 Double, V3 Double)
-solve v i
-  | abs d <= ε = (v, V3 gx gy gz)
-  | otherwise  = solve (v ^-^ V3 gx gy gz ^/ d) i
-  where d  = eval v i
-        gx = eval (v ^+^ (V3 ε 0 0)) i - d
-        gy = eval (v ^+^ (V3 0 ε 0)) i - d
-        gz = eval (v ^+^ (V3 0 0 ε)) i - d
+solve :: (V3 OurNumber -> OurNumber) -> V3 OurNumber -> (V3 OurNumber, V3 OurNumber)
+solve f v | abs d <= ε = (v, g)
+          | otherwise  = solve f (v ^-^ g ^* abs d / 2)
+  where !d = f v
+        !g = V3 (f (v ^+^ (V3 ε 0 0)) - d)
+                (f (v ^+^ (V3 0 ε 0)) - d)
+                (f (v ^+^ (V3 0 0 ε)) - d) ^/ ε
+
+
+-- NOTE: eval_model_handcoded is faster with these variables than it is with
+-- (spheres !! 0) index expressions. This seems counterintuitive to me;
+-- shouldn't those be compute-once?
+s1 = ISphere 5 1
+s2 = ISphere 4 2
+s3 = ISphere 3 3
+
+spheres = [s1, s2, s3]
+eval_model v = eval (Union spheres) v
+eval_model_mono v = eval_union_mono (Union spheres) v
+
+eval_model_handcoded v =
+  v1 `max` v2 `max` v3
+  where v1 = eval_sphere_handcoded s1 v
+        v2 = eval_sphere_handcoded s2 v
+        v3 = eval_sphere_handcoded s3 v
 
 
 main = defaultMain
   [
-    bench "eval sphere" $ nf (eval 0.1) (ISphere 5 0),
-    bench "eval sphere handcoded" $ nf (eval_sphere_handcoded 0.1) (ISphere 5 0)
+    bench "solve union" $ nf (solve eval_model) 0.1,
+    bench "solve union mono" $ nf (solve eval_model_mono) 0.1,
+    bench "solve union handcoded"
+      $ nf (solve eval_model_handcoded) 0.1
   ]
