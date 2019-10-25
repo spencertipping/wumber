e65b1c6dff37fa3bad4b5c7a38a2b901424f6e2d
Spencer Tipping <spencer.tipping@gmail.com>
2019-10-25 08:35:37 -0600

    Minor changes: starting to index the cost functions by dependencies so we get more efficient gradients

--- wumber/Wumber/Constraint.hs
+++ wumber/Wumber/Constraint.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -funbox-strict-fields #-}
 
 module Wumber.Constraint where
@@ -10,27 +12,25 @@ import Control.Monad.ST
 import Data.Array.ST
 import Data.Array.Unboxed
 import GHC.Float
+import Lens.Micro
+import Lens.Micro.TH
 
 
--- TODO
--- Is there any merit to having zero-crossings be a thing in constraints?
--- Implicits use negative values, and constraints seem like the type of thing
--- that might benefit from something similar.
---
--- This gives us a way to specify implicit constraint ranges, which may be of
--- some use.
-
-type Solution   = forall s. STUArray s Int Double
+type Solution s = STUArray s Int Double
+type CostFn     = ([Int], [Double] -> Double)
 data Constraint = HardConstant Int Double
                 | HardOffset   Int Int Double
-                | SoftCostFn   [Int] ([Double] -> Double)
+                | Soft         CostFn
 
 
 -- | Describes a set of constraints after we've propagated constants and
 --   offsets. '_cs_constants' maps each entry either to a finite Double value,
 --   or to NaN if the entry is variable.
 data ConstraintSet = CSet { _cs_constants :: UArray Int Double,
-                            _cs_fn        :: UArray Int Double -> Double }
+                            _cs_fn        :: forall s. Solution s -> ST s Double,
+                            _cs_fns       :: Array Int [CostFn] }
+
+makeLenses ''ConstraintSet
 
 
 nan :: Double
@@ -40,21 +40,18 @@ nan = 0/0
 ε = 1e-8
 
 
-var_set_ubound :: [Constraint] -> Int
-var_set_ubound cs = foldl1 max (map single_idx cs)
-  where single_idx (HardConstant i _) = i
-        single_idx (HardOffset i j _) = i `max` j
-        single_idx (SoftCostFn is _)  = foldl1 max is
-
-apply_cost_fn :: UArray Int Double -> Constraint -> Double
-apply_cost_fn s (SoftCostFn is f) = f $ map (s !) is
-apply_cost_fn _ _ = error "apply_cost_fn on a hard constraint"
-
 constant_fold :: [Constraint] -> ConstraintSet
-constant_fold cs = CSet consts eval
-  where fns    = filter (\case SoftCostFn _ _ -> True
-                               _              -> False) cs
-        eval s = sum $ map (apply_cost_fn s) fns
+constant_fold cs = CSet consts eval undefined
+  where eval s                         = sum <$> mapM (apply_cost_fn s) fns
+        apply_cost_fn s (Soft (is, f)) = f   <$> mapM (readArray s) is
+        fns                            = filter (\case Soft _ -> True
+                                                       _      -> False) cs
+
+        var_set_ubound cs = foldl1 max $ map single_idx cs
+          where single_idx (HardConstant i _) = i
+                single_idx (HardOffset i j _) = i `max` j
+                single_idx (Soft (is, _))     = foldl1 max is
+
         consts = runSTUArray do
           vs <- newArray (0, var_set_ubound cs) nan
           forM_ cs \case HardConstant i v -> writeArray vs i v
