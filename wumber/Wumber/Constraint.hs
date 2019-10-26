cd7b152c1ce89cdc31f202c6766c6f9a97b52356
Spencer Tipping <spencer.tipping@gmail.com>
2019-10-26 13:48:23 -0600

    Type parameters for CVals; this lets us use vectors

--- wumber/Wumber/Constraint.hs
+++ wumber/Wumber/Constraint.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE BlockArguments #-}
@@ -10,54 +11,69 @@ module Wumber.Constraint where
 import Control.Monad
 import Control.Monad.ST
 import Control.Monad.RWS
+import Data.Array.MArray
 import Data.Array.ST
 import Data.Array.Unboxed
 import GHC.Float
 import Lens.Micro
 import Lens.Micro.TH
+import Linear.V2
+import Linear.V3
 
 
 ε :: Double
 ε = 1e-8
 
 
--- | A constrained variable, constant, or a linear transformation of a
---   constrained thing.
-data CVal = CVar    !Int !Double
-          | CConst  !Double
-          | CLinear { _cl_m :: !Double, _cl_b :: !Double, _cl_v :: !CVal }
-          | CNonlinear { _cln_operands :: ![CVal],
-                         _cln_fn       :: !([Double] -> Double) }
+-- | A constrained variable, constant, or transformation of one or more such
+--   values. You build these up with numeric expressions and then assert
+--   equivalence using '===', which emits a constraint to the solver.
+data CVal a = CVar    !Int !a
+            | CConst  !a
+            | CLinear { _cl_m :: !a, _cl_b :: !a, _cl_v :: !(CVal a) }
+            | CNonlinear { _cln_operands :: ![CVal a],
+                           _cln_fn       :: !([a] -> a) }
 makeLenses ''CVal
+
+
+-- | Constraints are just values that are set to zero.
+type Constraint a = CVal a
+
+-- | Boolean 'and' (intersection) of a set of constraints.
+cand :: Ord a => [Constraint a] -> Constraint a
+cand xs = CNonlinear xs (foldl1 min)
+
+-- | Boolean 'or' (union) of a set of constraints.
+cor :: Ord a => [Constraint a] -> Constraint a
+cor xs = CNonlinear xs (foldl1 max)
+
 
 -- | 'Constrained' is a monad that keeps track of 'CVar' IDs and collects
 --   constraint expressions whose values should end up being zero. Constraints
 --   are solved in the 'ST' monad using mutable unboxed 'Double' arrays.
---
---   TODO: do we need RWST here, or is this RWS and then post-ST?
-type Constrained a = forall s. RWST () [CVal] Int (ST s) a
+type Constrained n a = RWS () [Constraint n] Int a
 
 
 -- | Apply a linear transformation to a single constrained value.
-linear :: Double -> Double -> CVal -> CVal
+linear :: Num a => a -> a -> CVal a -> CVal a
 linear m b (CConst x)        = CConst  (m * x + b)
 linear m b (CLinear m' b' v) = CLinear (m * m') (b + b') v
 linear m b v                 = CLinear m b v
 
 -- | Promote a unary function to a nonlinear transformation, with constant
 --   folding.
-nonlinear_unary :: (Double -> Double) -> CVal -> CVal
+nonlinear_unary :: Num a => (a -> a) -> CVal a -> CVal a
 nonlinear_unary f (CConst x) = CConst (f x)
 nonlinear_unary f v = CNonlinear [v] (f . head)
 
 -- | Promote a binary function to a nonlinear transformation, with constant
 --   folding.
-nonlinear_binary :: (Double -> Double -> Double) -> CVal -> CVal -> CVal
+nonlinear_binary :: Num a => (a -> a -> a) -> CVal a -> CVal a -> CVal a
 nonlinear_binary f (CConst x) (CConst y) = CConst (f x y)
 nonlinear_binary f x y = CNonlinear [x, y] (\[a, b] -> f a b)
 
 
-instance Num CVal where
+instance Num a => Num (CVal a) where
   fromInteger  = CConst . fromInteger
   negate       = linear (-1) 0
   CConst x + y = linear 1 x y
@@ -72,12 +88,12 @@ instance Num CVal where
   signum (CConst x) = CConst (signum x)
   signum v          = nonlinear_unary signum v
 
-instance Fractional CVal where
+instance Fractional a => Fractional (CVal a) where
   fromRational = CConst . fromRational
   recip (CConst x) = CConst (recip x)
   recip v          = nonlinear_unary recip v
 
-instance Floating CVal where
+instance Floating a => Floating (CVal a) where
   pi    = CConst pi
   exp   = nonlinear_unary exp
   log   = nonlinear_unary log
@@ -94,14 +110,13 @@ instance Floating CVal where
 
 
 -- | Create a new constrained variable initialized to the given value.
-var :: Double -> Constrained CVal
-var init = do
-  id <- get
-  modify (+ 1)
-  return $ CVar id init
+var :: a -> Constrained n (CVal a)
+var init = do id <- get
+              modify (+ 1)
+              return $ CVar id init
 
 
-eval :: forall s. CVal -> STUArray s Int Double -> ST s Double
+eval :: (MArray a e m, Num e) => CVal e -> a Int e -> m e
 eval (CVar i _)         xs = readArray xs i
 eval (CConst x)         _  = return x
 eval (CLinear m b v)    xs = do x <- eval v xs; return $ m*x + b
@@ -111,5 +126,5 @@ eval (CNonlinear ops f) xs = f <$> mapM (flip eval xs) ops
 -- | Specifies that two constrained expressions should have the same value. We
 --   assert this by creating a zero crossing when they are equal -- i.e. we
 --   subtract them.
-(===) :: CVal -> CVal -> Constrained ()
+(===) :: Num a => CVal a -> CVal a -> Constrained a ()
 a === b = tell [a - b]
