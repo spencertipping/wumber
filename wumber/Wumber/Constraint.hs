aa3d9b4055109788b1c712572c9ff25230dfe2f8
Spencer Tipping <spencer.tipping@gmail.com>
2019-10-26 08:42:16 -0600

    WIP rewriting the constraint system in a far more sane way

--- wumber/Wumber/Constraint.hs
+++ wumber/Wumber/Constraint.hs
@@ -9,6 +9,7 @@ module Wumber.Constraint where
 
 import Control.Monad
 import Control.Monad.ST
+import Control.Monad.RWS
 import Data.Array.ST
 import Data.Array.Unboxed
 import GHC.Float
@@ -16,49 +17,52 @@ import Lens.Micro
 import Lens.Micro.TH
 
 
-type Solution s = STUArray s Int Double
-type CostFn     = ([Int], [Double] -> Double)
-data Constraint = HardConstant Int Double
-                | HardOffset   Int Int Double
-                | Soft         CostFn
-
-
--- | Describes a set of constraints after we've propagated constants and
---   offsets. '_cs_constants' maps each entry either to a finite Double value,
---   or to NaN if the entry is variable.
-data ConstraintSet = CSet { _cs_constants :: UArray Int Double,
-                            _cs_fn        :: forall s. Solution s -> ST s Double,
-                            _cs_fns       :: Array Int [CostFn] }
-
-makeLenses ''ConstraintSet
-
-
-nan :: Double
-nan = 0/0
-
 ε :: Double
 ε = 1e-8
 
 
-constant_fold :: [Constraint] -> ConstraintSet
-constant_fold cs = CSet consts eval undefined
-  where eval s                         = sum <$> mapM (apply_cost_fn s) fns
-        apply_cost_fn s (Soft (is, f)) = f   <$> mapM (readArray s) is
-        fns                            = filter (\case Soft _ -> True
-                                                       _      -> False) cs
+-- | A constrained variable, constant, or a linear transformation of a
+--   constrained thing.
+data CVal = CVar    Int Double
+          | CConst  Double
+          | CLinear { _cl_m :: Double, _cl_b :: Double, _cl_v :: CVal }
+  deriving (Show, Read, Eq, Ord)
 
-        var_set_ubound cs = foldl1 max $ map single_idx cs
-          where single_idx (HardConstant i _) = i
-                single_idx (HardOffset i j _) = i `max` j
-                single_idx (Soft (is, _))     = foldl1 max is
+type Constraint    = ([CVal], [Double] -> Double)
+type Constrained a = forall s. RWST () [Constraint] Int (ST s) a
 
-        consts = runSTUArray do
-          vs <- newArray (0, var_set_ubound cs) nan
-          forM_ cs \case HardConstant i v -> writeArray vs i v
-                         _                -> return ()
 
-          forM_ cs \case HardOffset i j o -> do
-                           v <- readArray vs j
-                           unless (isNaN v) $ writeArray vs i (v + o)
-                         _                -> return ()
-          return vs
+-- | Apply a linear transformation to a single constrained value.
+linear :: Double -> Double -> CVal -> CVal
+linear m b (CConst x)        = CConst  (m * x + b)
+linear m b (CLinear m' b' v) = CLinear (m * m') (b + b') v
+linear m b v                 = CLinear m b v
+
+
+instance Num CVal where
+  fromInteger  = CConst . fromInteger
+  negate       = linear (-1) 0
+  CConst x + y = linear 1 x y
+  x + CConst y = linear 1 y x
+  CConst x * y = linear x 0 y
+  x * CConst y = linear y 0 x
+
+  abs (CConst x)    = CConst (abs x)
+  abs v             = error ("abs(" ++ show v ++ ") is nonlinear")
+  signum (CConst x) = CConst (signum x)
+  signum v          = error ("signum(" ++ show v ++ ") is nonlinear")
+
+instance Fractional CVal where
+  fromRational = CConst . fromRational
+  recip (CConst x) = CConst (recip x)
+  recip v          = error ("recip(" ++ show v ++ ") is nonlinear")
+
+
+-- | Create a new constrained variable initialized to the given value.
+var :: Double -> Constrained CVal
+var init = do
+  id <- get
+  modify (+ 1)
+  return $ CVar id init
+
+
