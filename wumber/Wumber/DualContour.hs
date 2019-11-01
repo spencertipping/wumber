0c5fbd912374eb7e188b1b407e85b787f317b0f4
Spencer Tipping <spencer.tipping@gmail.com>
2019-11-01 05:21:06 -0600

    WIP dual contour implementation, basically a direct transcription from Matt Keeter

--- wumber/Wumber/DualContour.hs
+++ wumber/Wumber/DualContour.hs
@@ -15,7 +15,10 @@ import Lens.Micro
 import Lens.Micro.TH
 import Linear.V3
 
+import Wumber.BoundingBox
 
+
+type IsoFn = V3 Double -> Double
 
 
 -- TODO
@@ -29,8 +32,48 @@ data Tree a = Root { _txyz :: Tree a, _txyZ :: Tree a,
             | Empty
             | Full
             | Leaf a
+  deriving (Show, Eq)
 
 makeLenses ''Tree
 
 
-build_tree :: BoundingBox
+build_tree :: BB3D -> Int -> Tree BB3D
+build_tree b 0 = Leaf b
+build_tree (BB (V3 xl yl zl) (V3 xu yu zu)) i =
+  Root (build_tree (BB (V3 xl yl zl) (V3 xm ym zm)) (i-1))
+       (build_tree (BB (V3 xl yl zm) (V3 xm ym zu)) (i-1))
+       (build_tree (BB (V3 xl ym zl) (V3 xm yu zm)) (i-1))
+       (build_tree (BB (V3 xl ym zm) (V3 xm yu zu)) (i-1))
+       (build_tree (BB (V3 xm yl zl) (V3 xu ym zm)) (i-1))
+       (build_tree (BB (V3 xm yl zm) (V3 xu ym zu)) (i-1))
+       (build_tree (BB (V3 xm ym zl) (V3 xu yu zm)) (i-1))
+       (build_tree (BB (V3 xm ym zm) (V3 xu yu zu)) (i-1))
+  where xm = (xl + xu) / 2
+        ym = (yl + yu) / 2
+        zm = (zl + zu) / 2
+
+
+collapse :: IsoFn -> Tree BB3D -> Tree BB3D
+collapse i l@(Leaf (BB (V3 xl yl zl) (V3 xu yu zu)))
+  | all (<= 0) corners = Empty
+  | all (>  0) corners = Full
+  | otherwise          = l
+  where corners = map i [V3 x y z | x <- [xl, xu], y <- [yl, yu], z <- [zl, zu]]
+
+collapse i (Root a b c d e f g h)
+  | all (== Empty) s' = Empty
+  | all (== Full)  s' = Full
+  | otherwise         = let [a', b', c', d', e', f', g', h'] = s' in
+                          Root a' b' c' d' e' f' g' h'
+  where s' = map (collapse i) [a, b, c, d, e, f, g, h]
+
+collapse _ t = t
+
+
+δ = 2**(-12)
+
+gradient :: IsoFn -> V3 Double -> V3 Double
+gradient i (V3 x y z) = V3 (gx/δ) (gy/δ) (gz/δ)
+  where gx = i (V3 (x+δ) y z) - i (V3 (x-δ) y z)
+        gy = i (V3 x (y+δ) z) - i (V3 x (y-δ) z)
+        gz = i (V3 x y (z+δ)) - i (V3 x y (z-δ))
