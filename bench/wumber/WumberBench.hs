73052e5bc93a38ab73365f4114cbe00d6e667c8b
Spencer Tipping <spencer.tipping@gmail.com>
2019-11-06 06:47:50 -0700

    Testing constant overhead: essentially zero

--- bench/wumber/WumberBench.hs
+++ bench/wumber/WumberBench.hs
@@ -18,12 +18,20 @@ handcoded_sphere r x y z (vx, vy, vz) = r - sqrt (dx*dx + dy*dy + dz*dz)
         dz = vz - z
 
 
+handcoded_const_sphere :: HandcodedIsoFn
+handcoded_const_sphere (x, y, z) = 2 - sqrt (dx*dx + dy*dy + dz*dz)
+  where dx = x - 0.5
+        dy = y - 1
+        dz = z - 2
+
+
 vector_sphere :: Double -> V3 Double -> VectorIsoFn
 vector_sphere r l v = r - distance l v
 
 
 main = defaultMain
   [
-    bench "handcoded sphere" (nf (handcoded_sphere 2     0 1 2)   (1, 2, 3)),
-    bench "vector sphere"    (nf (vector_sphere    2 (V3 0 1 2)) (V3 1 2 3))
+    bench "handcoded const sphere" (nf handcoded_const_sphere (1, 2, 3)),
+    bench "handcoded sphere" (nf (handcoded_sphere 2     0.5 1 2)   (1, 2, 3)),
+    bench "vector sphere"    (nf (vector_sphere    2 (V3 0.5 1 2)) (V3 1 2 3))
   ]
