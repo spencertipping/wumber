f8568f04958859f8060da812a67429e97707eb3e
Spencer Tipping <spencer.tipping@gmail.com>
2019-11-06 07:08:57 -0700

    Wow, I am very impressed: applicative cube performs _identically_ to handcoded and 20% faster than foldl, even without a SPECIALIZE pragma.

--- bench/wumber/WumberBench.hs
+++ bench/wumber/WumberBench.hs
@@ -1,7 +1,9 @@
 module Main where
 
+import Control.Applicative
 import Criterion
 import Criterion.Main
+import Data.Foldable
 
 import Linear.Metric
 import Linear.V3
@@ -36,10 +38,35 @@ handcoded_vector_sphere r (V3 x y z) (V3 vx vy vz) = r - sqrt (dx*dx + dy*dy + d
         dz = vz - z
 
 
+handcoded_vector_cube :: V3 Double -> V3 Double -> VectorIsoFn
+handcoded_vector_cube (V3 x1 y1 z1) (V3 x2 y2 z2) (V3 x y z) =
+  min (min (x - x1) (x2 - x))
+      (min (min (y - y1) (y2 - y))
+           (min (z - z1) (z2 - z)))
+
+
+fold_vector_cube :: V3 Double -> V3 Double -> VectorIsoFn
+fold_vector_cube (V3 x1 y1 z1) (V3 x2 y2 z2) (V3 x y z) =
+  foldl' min (1/0) [ x - x1, x2 - x, y - y1, y2 - y, z - z1, z2 - z ]
+
+
+app_vector_cube :: (Applicative f, Foldable f)
+                => f Double -> f Double -> f Double -> Double
+app_vector_cube a b x = foldl' min (1/0) $ liftA2 min lower upper
+  where lower = liftA2 (-) x a
+        upper = liftA2 (-) b x
+
+-- {-# SPECIALIZE app_vector_cube :: V3 Double -> V3 Double -> V3 Double -> Double #-}
+
+
 main = defaultMain
   [
-    bench "handcoded const sphere" (nf handcoded_const_sphere (1, 2, 3)),
-    bench "handcoded sphere" (nf (handcoded_sphere 2     0.5 1 2)   (1, 2, 3)),
-    bench "handcoded vector sphere" (nf (handcoded_vector_sphere 2 (V3 0.5 1 2)) (V3 1 2 3)),
-    bench "vector sphere"    (nf (vector_sphere    2 (V3 0.5 1 2)) (V3 1 2 3))
+    bench "HC sphere" (nf handcoded_const_sphere                    (1, 2, 3)),
+    bench "H  sphere" (nf (handcoded_sphere        2     0.5 1 2)   (1, 2, 3)),
+    bench "HV sphere" (nf (handcoded_vector_sphere 2 (V3 0.5 1 2)) (V3 1 2 3)),
+    bench "V  sphere" (nf (vector_sphere           2 (V3 0.5 1 2)) (V3 1 2 3)),
+
+    bench "HV cube"   (nf (handcoded_vector_cube (V3 1 2 3) (V3 4 5 6)) (V3 7 8 9)),
+    bench "FV cube"   (nf (fold_vector_cube      (V3 1 2 3) (V3 4 5 6)) (V3 7 8 9)),
+    bench "AV cube"   (nf (app_vector_cube       (V3 1 2 3) (V3 4 5 6)) (V3 7 8 9))
   ]
