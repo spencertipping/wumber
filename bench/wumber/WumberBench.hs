44582cf38bc873d4ea0d1167874dac43b71225df
Spencer Tipping <spencer.tipping@gmail.com>
2019-11-06 06:51:03 -0700

    V3 data structure overhead is about 10%, pretty minimal. Most of the inefficiency comes from Linear.Metric.

--- bench/wumber/WumberBench.hs
+++ bench/wumber/WumberBench.hs
@@ -29,9 +29,17 @@ vector_sphere :: Double -> V3 Double -> VectorIsoFn
 vector_sphere r l v = r - distance l v
 
 
+handcoded_vector_sphere :: Double -> V3 Double -> VectorIsoFn
+handcoded_vector_sphere r (V3 x y z) (V3 vx vy vz) = r - sqrt (dx*dx + dy*dy + dz*dz)
+  where dx = vx - x
+        dy = vy - y
+        dz = vz - z
+
+
 main = defaultMain
   [
     bench "handcoded const sphere" (nf handcoded_const_sphere (1, 2, 3)),
     bench "handcoded sphere" (nf (handcoded_sphere 2     0.5 1 2)   (1, 2, 3)),
+    bench "handcoded vector sphere" (nf (handcoded_vector_sphere 2 (V3 0.5 1 2)) (V3 1 2 3)),
     bench "vector sphere"    (nf (vector_sphere    2 (V3 0.5 1 2)) (V3 1 2 3))
   ]
