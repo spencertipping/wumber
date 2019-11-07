a98424776ec84cbdfbff9b65f63391cd40a65606
Spencer Tipping <spencer.tipping@gmail.com>
2019-11-07 12:06:57 -0700

    AWW YEAH - calls into math work as expected

--- wumber/Wumber/JIT.hs
+++ wumber/Wumber/JIT.hs
@@ -6,6 +6,8 @@ module Wumber.JIT where
 
 import Control.Monad
 import Data.ByteString (ByteString, pack)
+import Data.ByteString.Builder
+import Data.ByteString.Lazy (toStrict)
 import Data.ByteString.Unsafe (unsafeUseAsCStringLen)
 import Data.Vector.Storable (Vector, empty, fromList, unsafeWith)
 import Foreign.C.Types
@@ -24,6 +26,10 @@ foreign import ccall unsafe "sys/mman.h mmap"
 
 foreign import ccall unsafe "sys/mman.h munmap"
   munmap :: Ptr () -> CSize -> IO CInt
+
+-- Just making sure this does what we expect
+foreign import ccall unsafe "math.h &sin"
+  p_sin :: FunPtr (Double -> IO Double)
 
 foreign import ccall "dynamic"
   jit_dblfn :: FunPtr (Ptr a -> IO Double) -> Ptr a -> IO Double
@@ -49,9 +55,15 @@ with_jit_dbl code f = do
 
 
 test_fn :: ByteString
-test_fn = pack [ 0xf2, 0x0f, 0x10, 0x47, 0x08,   -- movsd 8(%rdi), %xmm0
-                 0xf2, 0x0f, 0x58, 0xc0,         -- addsd %xmm0, %xmm0
-                 0xc3 ]                          -- ret
+test_fn = toStrict $ toLazyByteString $
+  mconcat (map word8 [ 0xc8, 0x00, 0x00, 0x00,         -- enter 0 0
+                       0xf2, 0x0f, 0x10, 0x47, 0x08,   -- movsd 8(%rdi), %xmm0
+                       0xf2, 0x0f, 0x58, 0xc0,         -- addsd %xmm0, %xmm0
+                       0x48, 0xb8 ])                   -- movq %rax,
+  <> word64LE (unsafeCoerce p_sin)                     --      imm64
+  <> mconcat (map word8 [ 0xff, 0xd0,                  -- call %rax
+                          0xc9,                        -- leave
+                          0xc3 ])                      -- ret
 
 test :: Double -> IO Double
 test x = with_jit_dbl test_fn \f -> return $! f (fromList [100, x])
