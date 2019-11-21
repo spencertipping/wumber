7cd4aae7d22e93f53567052c79c818ee0a7fffba
Spencer Tipping <spencer.tipping@gmail.com>
2019-11-21 05:14:06 -0700

    Minor cleanup

--- wumber/Wumber/ConstraintSolver.hs
+++ wumber/Wumber/ConstraintSolver.hs
@@ -9,7 +9,13 @@
 
 {-# OPTIONS_GHC -funbox-strict-fields #-}
 
-module Wumber.ConstraintSolver where
+-- | A hybrid algebraic and numerical constraint system solver.
+module Wumber.ConstraintSolver (
+  solve,
+  solve_full,
+  Rewritable(..),
+  constraint_cost
+) where
 
 
 import Control.Monad.RWS        (evalRWS)
@@ -29,6 +35,29 @@ import Wumber.Symbolic
 import Wumber.SymbolicJIT
 
 
+-- | Solves a constrained system that returns a rewritable value, and rewrites
+--   that value with the constraint solution.
+--
+--   Inputs are the desired delta, the maximum number of iterations for the
+--   solver to use, and the constraint system you want to solve. Internally,
+--   'solve' identifies independent subsystems and applies algebraic
+--   simplification to each one before handing things off to the numerical
+--   minimizer. The goal is to minimize the amount of work required for complex
+--   constraint sets.
+solve :: Rewritable a b => R -> Int -> Constrained a -> b
+solve δ n m = b where (b, _, _) = solve_full δ n m
+
+
+-- | Like 'solve', but returns the solution vector and constraints alongside the
+--   result. This is useful if you want to verify tolerances.
+solve_full :: Rewritable a b
+           => R -> Int -> Constrained a -> (b, Vector R, [Constraint])
+solve_full δ n m = (rewrite (eval (solution !)) a, solution, cs)
+  where solution = VS.replicate (1 + foldl1 max (map fst solved)) 0 VS.// solved
+        (a, cs)  = evalRWS m () 0
+        solved   = concatMap (solve' δ n) (subsystems cs)
+
+
 -- | The class of objects that can have constraint variables rewritten into
 --   fixed values. Not all objects will preserve form when you do this; you
 --   might start with constraint-friendly data structures that render themselves
@@ -44,22 +73,6 @@ instance Functor f => Rewritable (f CVal) (f R) where
   rewrite = fmap . rewrite
 
 
--- | Solves a constrained system that returns a rewritable value, and rewrites
---   that value with the constraint solution.
-solve :: Rewritable a b => R -> Int -> Constrained a -> b
-solve δ n m = b where (b, _, _) = solve_full δ n m
-
-
--- | Like 'solve', but returns the solution vector and constraints alongside the
---   result. This is useful if you want to verify tolerances.
-solve_full :: Rewritable a b
-           => R -> Int -> Constrained a -> (b, Vector R, [Constraint])
-solve_full δ n m = (rewrite (eval (solution !)) a, solution, cs)
-  where solution = VS.replicate (1 + foldl1 max (map fst solved)) 0 VS.// solved
-        (a, cs)  = evalRWS m () 0
-        solved   = concatMap (solve' δ n) (subsystems cs)
-
-
 -- | Solves a constrained system and returns the solution as a list of
 --   '(Int, N)' tuples. This is a low-level function used by 'solve'.
 solve' :: R -> Int -> Subsystem -> [(Int, R)]
@@ -69,8 +82,10 @@ solve' δ n (Subsystem cs mi start) = remap_solution mi xs
         search_size = VS.replicate (V.length mi) 1
 
 
--- | The cost function for a set of constraints at a given solution value.
-constraint_cost :: [Constraint] -> CVal
+-- | The total cost for a set of constraints. It's ok for this to be slow; the
+--   goal is to get a symbolic quantity we can JIT into the cost function we
+--   send to the minimizer.
+constraint_cost :: [Constraint] -> Sym R
 constraint_cost cs = sum $ concatMap each cs
   where each (CEqual a b)      = [(a - b) ** 2]
         each (CMinimize v)     = [upper 0 v]
