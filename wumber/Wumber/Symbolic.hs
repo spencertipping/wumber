99857727079d815134c01b11d75f9a557f708a2d
Spencer Tipping <spencer.tipping@gmail.com>
2019-11-23 03:50:24 -0700

    Basically-working polynomial stuff. It seems to reduce terms decently, although I suspect there are holes.

--- wumber/Wumber/Symbolic.hs
+++ wumber/Wumber/Symbolic.hs
@@ -35,12 +35,13 @@ module Wumber.Symbolic (
   Mod(..),
   Functionable(..),
   eval,
-  args_in,
+  vars_in,
 ) where
 
 
 import Data.Binary  (Binary(..))
-import Data.Set     (Set(..), empty, singleton, union)
+import Data.List    (sort)
+import Data.Set     (Set(..), empty, singleton, union, unions)
 import Data.Vector  (Vector, (!))
 import Foreign.Ptr  (FunPtr(..))
 import GHC.Generics (Generic(..))
@@ -63,7 +64,7 @@ data Sym a = [SymTerm a] :+ a
   deriving (Show, Ord, Eq, Generic, Binary)
 
 data SymTerm a = a :* [SymExp a]
-  deriving (Show, Eq, Generic, Binary)
+  deriving (Show, Ord, Eq, Generic, Binary)
 
 data SymExp a = SymVar a :** a
   deriving (Show, Ord, Eq, Generic, Binary)
@@ -81,44 +82,70 @@ infixl 7 :*
 infixl 8 :**
 
 
-instance Ord a => Ord (SymTerm a) where
-  (_ :* a) `compare` (_ :* b) = a `compare` b
+-- | Evaluate a symbolic quantity using Haskell math. To do this, we need a
+--   function that handles 'Arg' values.
+--
+--   TODO: remove RealFloat constraint, or have Sym implement RealFloat. I want
+--   to be able to use 'eval' to rewrite things.
+
+eval :: (Floating a, RealFloat a, Mod a, ClosedComparable a)
+     => (VarID -> a) -> Sym a -> a
+
+eval f (ts :+ b) = b + sum (map eval_t ts)
+  where eval_t (a :* xs)      = a * product (map eval_e xs)
+        eval_e (x :** n)      = eval_v x ** n
+        eval_v (Var i)        = f i
+        eval_v (Fn1 op _ x)   = fn op (eval f x)
+        eval_v (Fn2 op _ x y) = fn op (eval f x) (eval f y)
 
 
-p0 :: a -> Sym a
-p0 = (:+ [])
+p0   = ([] :+)
+t0   = (:* [])
+vp x = [1 :* [x :** 1]] :+ 0
 
-pnull :: (Eq a, Num a) => Sym a -> Bool
-pnull ([] :+ 0) = True
-pnull _         = False
+fn1 f x   = Fn1 f (vars_in x) x
+fn2 f x y = Fn2 f (vars_in x `union` vars_in y) x y
+
+pt (xs :+ _) = xs
+te (_ :* es) = es
+ev (v :** _) = v
 
 
-merge_with :: Ord a => (a -> b) -> (a -> a -> b) -> [a] -> [a] -> [b]
-merge_with i f xs [] = map i xs
-merge_with i f [] ys = map i ys
-merge_with i f (x:xs) (y:ys) | x < y     = i x   : merge_with i f xs (y:ys)
-                             | y < x     = i y   : merge_with i f (x:xs) ys
-                             | otherwise = f x y : merge_with i f xs ys
+merge_with :: Ord o => (a -> o) -> (a -> b) -> (a -> a -> b) -> [a] -> [a] -> [b]
+merge_with _ i _ xs [] = map i xs
+merge_with _ i _ [] ys = map i ys
+merge_with v i f (x:xs) (y:ys) | v x < v y = i x   : merge_with v i f xs (y:ys)
+                               | v y < v x = i y   : merge_with v i f (x:xs) ys
+                               | otherwise = f x y : merge_with v i f xs ys
 
 
 padd :: (Eq a, Num a, Ord a) => Sym a -> Sym a -> Sym a
-padd (xs :+ a) (ys :+ b) = concat (merge_with (: []) tadd xs ys) :+ (a + b)
+padd (xs :+ a) (ys :+ b) = concat (merge_with te (: []) tadd xs ys) :+ (a + b)
   where tadd (a :* x) (b :* _) | a + b /= 0 = [(a + b) :* x]
                                | otherwise  = []
 
-pmul :: (Eq a, Num a, Ord a) => Sym a -> Sym a -> Sym a
-pmul (xs :+ a) (ys :+ b) = sum (p0 (a * b) : [tmul x y :+ 0 | x <- xs, y <- ys])
+pmul :: (Num a, Ord a) => Sym a -> Sym a -> Sym a
+pmul (xs :+ a) (ys :+ b) = sum [tmul x y | x <- t0 a : xs, y <- t0 b : ys]
 
+tmul :: (Num a, Ord a) => SymTerm a -> SymTerm a -> Sym a
+tmul (a :* xs) (b :* ys) | null es    = []              :+ a * b
+                         | a * b /= 0 = [(a * b) :* es] :+ 0
+                         | otherwise  = 0
+  where es = concat (merge_with ev (: []) emul xs ys)
 
-  
--- tmul :: 
+emul :: (Num a, Ord a) => SymExp a -> SymExp a -> [SymExp a]
+emul (x :** m) (y :** n) | x /= y     = sort [x :** m, y :** n]
+                         | m + n /= 0 = [x :** (m + n)]
+                         | otherwise  = []
 
 
 instance (Ord a, Num a) => Num (Sym a) where
   fromInteger = p0 . fromInteger
-  (+)   = padd
-  a - b = a + (-1) * b
-  (*)   = pmul
+  abs         = vp . fn1 Abs
+  signum      = vp . fn1 Signum
+  negate      = ((-1) *)
+  (+)         = padd
+  (*)         = pmul
 
 
 -- | Unary transcendental functions that would otherwise clutter up 'Sym'.
@@ -141,27 +168,11 @@ data SymFn1 = Abs
   deriving (Show, Ord, Eq, Generic, Binary, Enum)
 
 data SymFn2 = Mod
+            | Pow
             | Upper
             | Lower
             | Atan2
   deriving (Show, Ord, Eq, Generic, Binary, Enum)
-
-
--- | Evaluate a symbolic quantity using Haskell math. To do this, we need a
---   function that handles 'Arg' values.
---
---   TODO: remove RealFloat constraint, or have Sym implement RealFloat. I want
---   to be able to use 'eval' to rewrite things.
-
-eval :: (Floating a, RealFloat a, Mod a, ClosedComparable a)
-     => (VarID -> a) -> Sym a -> a
-
-eval f (ts :+ b) = b + sum (map eval_t ts)
-  where eval_t (Term a x n)    = a * eval_symt x ** n
-
-        eval_symt (Var i)      = f i
-        eval_symt (Fn1 op x)   = fn op (eval f x)
-        eval_symt (Fn2 op x y) = fn op (eval f x) (eval f y)
 
 
 class Functionable x t | t -> x where
@@ -188,6 +199,7 @@ instance (Floating a, Mod a) => Functionable SymFn1 (a -> a) where
 instance (Floating a, Mod a, ClosedComparable a, RealFloat a) =>
          Functionable SymFn2 (a -> a -> a) where
   fn Mod      = (%)
+  fn Pow      = (**)
   fn Upper    = nan_upper
   fn Lower    = nan_lower
   fn Atan2    = atan2
@@ -204,10 +216,12 @@ nan_lower x y | isNaN x = x
 
 -- | Returns a set of all 'Var' indexes used by an expression.
 vars_in :: Sym a -> Set VarID
-vars_in (Poly _ xs _ _) = unions (map vars_in_t xs)
-  where vars_in_t (Term _ (Var x)      _) = singleton x
-        vars_in_t (Term _ (Fn1 vs _)   _) = vs
-        vars_in_t (Term _ (Fn2 vs _ _) _) = vs
+vars_in (xs :+ b) = unions (map vars_in_t xs)
+  where vars_in_t (a :* xs)     = unions (map vars_in_e xs)
+        vars_in_e (x :** n)     = vars_in_v x
+        vars_in_v (Var i)       = singleton i
+        vars_in_v (Fn1 _ v _)   = v
+        vars_in_v (Fn2 _ v _ _) = v
 
 
 -- | Values that support floating-point 'mod', but without using Haskell's
@@ -232,37 +246,39 @@ foreign import ccall unsafe "math.h fmodf" c_fmodf :: Float  -> Float  -> Float
 
 
 instance Bounded a => Bounded (Sym a) where
-  minBound = N minBound
-  maxBound = N maxBound
+  minBound = p0 minBound
+  maxBound = p0 maxBound
 
-instance Fractional a => Fractional (Sym a) where
-  fromRational = N . fromRational
-  (/) = cf_binary (/) (Fn2 Divide)
+instance (Ord a, Floating a, Fractional a) => Fractional (Sym a) where
+  fromRational = p0 . fromRational
+  recip        = (** (-1))
 
-instance Mod a => Mod (Sym a) where
-  (%) = cf_binary (%) (Fn2 Mod)
+instance (Ord a, Mod a, Num a) => Mod (Sym a) where
+  a % b | a == b    = 0
+        | otherwise = vp $ fn2 Mod a b
 
-instance Floating a => Floating (Sym a) where
-  pi = N pi
+instance (Ord a, Floating a) => Floating (Sym a) where
+  pi = p0 pi
 
-  (**) = cf_binary (**) (Fn2 Pow)
+  ([]        :+ x) ** ([] :+ n) = [] :+ x ** n
+  ([a :* es] :+ 0) ** ([] :+ n) = [a**n :* map (\(v:**e) -> v:**(e*n)) es] :+ 0
+  a                ** b         = vp $ fn2 Pow a b
 
-  exp   = cf_unary exp   (Fn1 Exp)
-  log   = cf_unary log   (Fn1 Log)
-  sqrt  = cf_unary sqrt  (Fn1 Sqrt)
-  sin   = cf_unary sin   (Fn1 Sin)
-  cos   = cf_unary cos   (Fn1 Cos)
-  tan   = cf_unary tan   (Fn1 Tan)
-  asin  = cf_unary asin  (Fn1 Asin)
-  acos  = cf_unary acos  (Fn1 Acos)
-  atan  = cf_unary atan  (Fn1 Atan)
-  sinh  = cf_unary sinh  (Fn1 Sinh)
-  cosh  = cf_unary cosh  (Fn1 Cosh)
-  tanh  = cf_unary tanh  (Fn1 Tanh)
-  asinh = cf_unary asinh (Fn1 Asinh)
-  acosh = cf_unary acosh (Fn1 Acosh)
-  atanh = cf_unary atanh (Fn1 Atanh)
+  exp   = vp . fn1 Exp
+  log   = vp . fn1 Log
+  sin   = vp . fn1 Sin
+  cos   = vp . fn1 Cos
+  tan   = vp . fn1 Tan
+  asin  = vp . fn1 Asin
+  acos  = vp . fn1 Acos
+  atan  = vp . fn1 Atan
+  sinh  = vp . fn1 Sinh
+  cosh  = vp . fn1 Cosh
+  tanh  = vp . fn1 Tanh
+  asinh = vp . fn1 Asinh
+  acosh = vp . fn1 Acosh
+  atanh = vp . fn1 Atanh
 
-instance ClosedComparable a => ClosedComparable (Sym a) where
-  lower = cf_binary lower (Fn2 Lower)
-  upper = cf_binary upper (Fn2 Upper)
+instance (Num a, ClosedComparable a) => ClosedComparable (Sym a) where
+  lower a b = vp $ fn2 Lower a b
+  upper a b = vp $ fn2 Upper a b
