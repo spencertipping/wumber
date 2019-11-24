60335795822673605712962c2046567f6bb1619b
Spencer Tipping <spencer.tipping@gmail.com>
2019-11-24 14:48:28 -0700

    OK, this basic structure makes sense to me.

--- wumber/Wumber/JITIR.hs
+++ wumber/Wumber/JITIR.hs
@@ -16,23 +16,26 @@
 --   register and intermittently uses a second register for operands. In theory,
 --   a processor with /n/ registers can run /n - 1/ threads without spilling.
 
-module Wumber.JITIR (
-  Thread(..),
-  Insn(..),
-  thread
-) where
+module Wumber.JITIR where
 
+
+import qualified Data.Vector as V
 
 import Wumber.Symbolic
 
 
 -- | A series of transformations to a single initial value.
-data Thread a = Thr !(Either a VarID) [Insn a] deriving (Eq, Show)
+data Thread a = Thr !(ThreadSource a) [Insn a] deriving (Eq, Show)
+data ThreadSource a = Val      !a
+                    | ReadVar  !VarID
+                    | Register !RegID
+  deriving (Eq, Show)
+
+type RegID = Int
 
 -- | A single transformation within a thread.
-data Insn a = I1  SymFn1
-            | I2L SymFn2 !(Thread a)  -- Accumulated value is on the left
-            | I2R SymFn2 !(Thread a)  -- Accumulated value is on the right
+data Insn a = I1 !SymFn1
+            | I2 !SymFn2 !(Thread a)
   deriving (Eq, Show)
 
 
@@ -40,24 +43,42 @@ data Insn a = I1  SymFn1
 thread :: SymConstraints f a => Sym f a -> Thread a
 thread s = case s of
   [t] :+ 0 -> tt t
-  [t] :+ n -> tt t $+ [I2L Add (Thr (Left n) [])]
-  ts  :+ n -> Thr (Left n) (map (I2L Add . tt) ts)
+  [t] :+ n -> tt t $+ [I2 Add (Thr (Val n) [])]
+  ts  :+ n -> Thr (Val n) (map (I2 Add . tt) ts)
 
   where tt (1 :* [e]) = et e
-        tt (a :* [e]) = et e $+ [I2L Multiply (Thr (Left a) [])]
-        tt (a :* es)  = Thr (Left a) (map (I2L Multiply . et) es)
+        tt (a :* [e]) = et e $+ [I2 Multiply (Thr (Val a) [])]
+        tt (a :* es)  = Thr (Val a) (map (I2 Multiply . et) es)
         et (x :** 1)  = vt x
-        et (x :** n)  = Thr (Left n) [I2R Pow (vt x)]
+        et (x :** n)  = vt x $+ [I2 Pow (Thr (Val n) [])]
 
-        vt (Var i)                 = Thr (Right i) []
+        vt (Var i)                 = Thr (ReadVar i) []
         vt (Fn1 f _ (OS a))        = thread a $+ [I1 f]
-        vt (Fn2 f _ (OS a) (OS b)) = thread a $+ [I2L f (thread b)]
+        vt (Fn2 f _ (OS a) (OS b)) = thread a $+ [I2 f (thread b)]
         -- TODO: FnN
 
         Thr i xs $+ ys = Thr i (xs ++ ys)
 
 
--- TODO
--- We need a thread traversal thing that can keep track of register allocations,
--- or at the very least can DFS-with-lookahead. A lot of this logic will be
--- shared across multiple JIT backends.
+-- | Returns the next step and continuation for a thread, or the location of the
+--   data if the thread is done.
+continuation :: RegID -> Thread a -> ThreadStep a
+continuation _ (Thr s [])     = Left s
+continuation r (Thr s (i:is)) = Right (s, i, Thr (Register r) is)
+
+type ThreadStep a = Either (ThreadSource a) (ThreadSource a, Insn a, Thread a)
+
+
+-- | Schedules the next instruction given the specified register constraints.
+--   The register index is indicated by the 'ThreadSource' in 'Step' or 'Done'.
+schedule :: V.Vector RegisterState -> Thread a -> Maybe (ThreadStep a, Thread a)
+schedule rs t = Nothing
+
+
+-- | Describes the state of a register: 'Free' (the scheduler can allocate it),
+--   'Running' (it's running an instruction that hasn't finished yet), or
+--   'Ready' (it contains a result).
+data RegisterState = Free
+                   | Running !Double
+                   | Ready
+  deriving (Eq, Show)
