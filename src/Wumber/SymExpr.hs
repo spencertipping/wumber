56862cce889d99619f260f04dddcecd1b443700f
Spencer Tipping <spencer.tipping@gmail.com>
2019-12-05 08:25:44 -0700

    Docs + rearranging

--- src/Wumber/SymExpr.hs
+++ src/Wumber/SymExpr.hs
@@ -6,7 +6,6 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE TemplateHaskell #-}
 
 -- NOTE
 -- The code will still compile and appear to work without this, but 'negate' and
@@ -25,17 +24,36 @@
 --   1. Its full 'BitSet' of variables
 --   2. A lazy 'Fingerprint' uniquely identifying it
 --   3. Its tree complexity, which is the total number of nodes underneath it
+--
+--   Of these, only the tree complexity is calculated eagerly.
+--
+--   Don't @import@ this module directly unless you're writing Wumber-internal
+--   code. Instead, import @Wumber@ to get relevant @Show@ instances and other
+--   helpful interfacing.
+
+-- TODO
+-- Add a condensed "profile" to quickly reject certain algebraic configurations
+-- without visiting subnodes
 
 module Wumber.SymExpr (
   Sym,
+  SymbolicApply(..),
+  ValApply(..),
   val,
-  var
+  var,
+  val_of,
+  vars_in,
+
+  eval_constant,
+  tree_size,
+
+  sym_apply_fold,
+  sym_apply_cons,
 ) where
 
 
 import Data.Foldable (foldl', toList)
 import GHC.Generics  (Generic)
-import Lens.Micro.TH (makeLenses)
 
 import Wumber.Fingerprint
 
@@ -64,13 +82,37 @@ instance (Fingerprintable f, Fingerprintable a) =>
   fingerprint (SymF _ _ i _ _) = i
 
 instance (Eq f, Eq a) => Eq (Sym f a) where
-  SymV i == SymV j = i == j
-  SymC a == SymC b = a == b
+  SymV i         == SymV j         = i == j
+  SymC a         == SymC b         = a == b
   SymF _ _ i _ _ == SymF _ _ j _ _ = i == j
-  _ == _ = False
+  _              == _              = False
 
 
-makeLenses ''Sym
+-- | Promotes a constant into a symbolic value.
+val :: a -> Sym f a
+val = SymC
+
+-- | Returns a symbolic quantity referring to the given indexed variable.
+var :: VarID -> Sym f a
+var = SymV
+
+
+-- | If the symbolic expression can be reduced to a constant value, returns
+--   'Just' that value. Otherwise returns 'Nothing'.
+--
+--   Symbolic expressions have constant values exactly when they refer to no
+--   variables.
+
+val_of :: ValApply f a => Sym f a -> Maybe a
+val_of s | BS.is_empty (vars_in s) = Just $ eval_constant s
+         | otherwise               = Nothing
+
+
+-- | Returns the set of variables referred to by the given tree.
+vars_in :: Sym f a -> BS.BitSet
+vars_in (SymV i) = BS.singleton i
+vars_in (SymC _) = BS.empty
+vars_in (SymF _ v _ _ _) = v
 
 
 -- | The class of functions that can be applied to symbolic arguments, yielding
@@ -79,7 +121,6 @@ makeLenses ''Sym
 --   commutative ones, will flatten the tree when possible.
 class SymbolicApply f where sym_apply :: f -> [Sym f a] -> Sym f a
 
-
 -- | Apply a function to symbolic quantities with constant folding.
 sym_apply_fold :: (Fingerprintable f, Fingerprintable (Sym f a), ValApply f a)
                => f -> [Sym f a] -> Sym f a
@@ -87,15 +128,22 @@ sym_apply_fold f xs
   | all (BS.is_empty . vars_in) xs = val (val_apply f (map eval_constant xs))
   | otherwise                      = sym_apply_cons f xs
 
-
 -- | This function conses a new tree node. This is the way you should implement
 --   'sym_apply' if no algebraic rules apply, and if constant folding isn't
---   possible/desirable.
+--   possible/desirable. (Otherwise you should back into 'sym_apply_fold'.)
 sym_apply_cons :: (Fingerprintable f, Fingerprintable (Sym f a))
                => f -> [Sym f a] -> Sym f a
-sym_apply_cons = sfn
+sym_apply_cons f xs = SymF f b id s v
+  where b  = BS.unions $ map vars_in xs
+        id = tree_fingerprint $ fingerprint f : map fingerprint xs
+        s  = 1 + sum (map tree_size xs)
+        v  = V.fromList xs
 
 
+-- | The class of functions that can be applied to values of type @a@, yielding
+--   another value of type @a@. If your function type implements 'sym_apply'
+--   using 'sym_apply_fold', then 'val_apply' will be used to handle constant
+--   folding.
 class ValApply f a where val_apply :: f -> [a] -> a
 
 instance ValApply (a -> a) a where
@@ -107,39 +155,18 @@ instance ValApply (a -> a -> a) a where
   val_apply _ xs     = error $ "wrong arity for binary fn: " ++ show (length xs)
 
 
+-- | An internal function that reduces constant 'Sym' expressions to their
+--   terminal values. This function is used by 'sym_apply_fold'.
 eval_constant :: ValApply f a => Sym f a -> a
 eval_constant (SymV i) = error $ "unexpected variable in constexpr " ++ show i
 eval_constant (SymC x) = x
 eval_constant (SymF f _ _ _ xs) = val_apply f (map eval_constant $ V.toList xs)
 
 
-val :: a -> Sym f a
-val = SymC
-
-var :: VarID -> Sym f a
-var = SymV
-
-val_of :: ValApply f a => Sym f a -> Maybe a
-val_of s | BS.is_empty (vars_in s) = Just $ eval_constant s
-         | otherwise               = Nothing
-
-
-sfn :: (Foldable l, Functor l, Fingerprintable f, Fingerprintable (Sym f a))
-    => f -> l (Sym f a) -> Sym f a
-sfn f xs = SymF f (BS.unions $ fmap vars_in xs)
-                  (tree_fingerprint [fingerprint f,
-                                     tree_fingerprint $ fmap fingerprint xs])
-                  (1 + sum (fmap tree_size xs))
-                  (V.fromList $ toList xs)
-
-
+-- | Returns the total number of elements in the given tree: nodes and
+--   functions. When algebraic rewriting provides multiple representation, we
+--   usually choose the one with the smallest 'tree_size'.
 tree_size :: Sym f a -> Int
 tree_size (SymV _) = 1
 tree_size (SymC _) = 1
 tree_size (SymF _ _ _ s _) = s
-
-
-vars_in :: Sym f a -> BS.BitSet
-vars_in (SymV i) = BS.singleton i
-vars_in (SymC _) = BS.empty
-vars_in (SymF _ v _ _ _) = v
