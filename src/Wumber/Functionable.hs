0248aabf1b9b43846c1fe2aaaab4154017199c20
Spencer Tipping <spencer.tipping@gmail.com>
2019-12-08 03:08:51 -0700

    Whew, got math automated (finally)

--- src/Wumber/Functionable.hs
+++ src/Wumber/Functionable.hs
@@ -15,10 +15,12 @@
 module Wumber.Functionable where
 
 
-import Data.Binary  (Binary)
-import Data.List    (intercalate)
-import GHC.Generics (Generic)
+import Data.Binary   (Binary)
+import Data.List     (intercalate)
+import GHC.Generics  (Generic)
+import Unsafe.Coerce (unsafeCoerce)
 
+import Wumber.ClosedComparable
 import Wumber.Fingerprint
 
 
@@ -167,25 +169,101 @@ instance MathFnC a => Functionable MathFn (Maybe (a -> a -> a -> a)) where
 -- | Semi-automatic Haskell math derivation for stuff. Implementing all of the
 --   instance functions by hand is a lot of work, so let's factor it all into a
 --   @newtype@ instead.
-newtype Math a b = Math { unMath :: a }
-  deriving (Eq, Ord, Generic, Binary)
+newtype Math a b = Math { unMath :: a } deriving (Generic, Binary)
 
-instance Show a => Show (Math a b) where show (Math x) = show x
+instance Eq (Math a b) where
+  _ == _ = error $ "Math instances are not comparable; use IfNN "
+           ++ "(or, for structural comparison, unwrap with unMath)"
 
-class MathApply a where
-  fn1 :: MathFn -> a -> a
-  fn2 :: MathFn -> a -> a -> a
+instance Ord (Math a b) where
+  compare _ _ = error $ "Math instances are not comparable; use IfNN "
+                ++ "(or, for structural comparison, unwrap with unMath)"
 
 
-fn1' f x   = Math $ fn1 f (unMath x)
-fn2' f x y = Math $ fn2 f (unMath x) (unMath y)
+class MathApply a m | m -> a where
+  fn0 :: a -> m
+  fn1 :: MathFn -> m -> m
+  fn2 :: MathFn -> m -> m -> m
+  fn3 :: MathFn -> m -> m -> m -> m
 
-instance (MathApply a, MathFnC a) => Num (Math a b) where
-  fromInteger = Math . fromInteger
-  (+)    = fn2' Add
-  (*)    = fn2' Mul
-  negate = fn1' Negate
-  signum = fn1' Signum
-  abs    = fn1' Abs
 
--- TODO: the rest of these
+instance (MathApply n (Math a b), MathFnC n) => Num (Math a b) where
+  fromInteger = fn0 . fromInteger
+  (+)    = fn2 Add
+  (*)    = fn2 Mul
+  negate = fn1 Negate
+  signum = fn1 Signum
+  abs    = fn1 Abs
+
+instance (MathApply n (Math a b), MathFnC n) => Fractional (Math a b) where
+  fromRational = fn0 . fromRational
+  recip        = fn1 Recip
+
+instance (MathApply n (Math a b), MathFnC n) => Floating (Math a b) where
+  pi    = fn0 pi
+  (**)  = fn2 Pow
+  exp   = fn1 Exp
+  log   = fn1 Log
+  sin   = fn1 Sin
+  cos   = fn1 Cos
+  tan   = fn1 Tan
+  asin  = fn1 Asin
+  acos  = fn1 Acos
+  atan  = fn1 Atan
+  sinh  = fn1 Sinh
+  cosh  = fn1 Cosh
+  tanh  = fn1 Tanh
+  asinh = fn1 Asinh
+  acosh = fn1 Acosh
+  atanh = fn1 Atanh
+
+instance (MathApply n (Math a b), MathFnC n) => ClosedComparable (Math a b) where
+  upper a b = fn3 IfNN (a - b) a b
+  lower a b = fn3 IfNN (a - b) b a
+
+
+instance (MathApply n (Math a b), MathFnC n) => Enum (Math a b) where
+  toEnum     = fromInteger . toInteger
+  fromEnum _ = error "can't collapse Math to Int via fromEnum"
+  pred x     = x - 1
+  succ x     = x + 1
+
+instance (MathApply n (Math a b), MathFnC n) => Real (Math a b) where
+  toRational _ = error "can't collapse Math to Rational via toRational"
+
+instance (MathApply n (Math a b), MathFnC n) => Integral (Math a b) where
+  toInteger _ = error "can't collapse Math to Integer; use Truncate"
+  quotRem a b = (fn2 Quot a b, fn2 Rem a b)
+  divMod a b  = (fn2 Div a b,  fn2 Mod a b)
+
+
+-- | A quotient/remainder function we reuse in several places.
+--
+--   NOTE: this results in 'divMod' and 'quotRem' that behave identically,
+--   including for negative divisors. This is technically not the way 'mod' and
+--   'rem' should be defined, but it lets us keep things simple for now.
+--
+--   TODO: is the fix as simple as having 'qr' use 'truncate' and 'divmod' use
+--   'floor'?
+
+qr a b = (q, r) where q = truncate (a / b); r = a - q*b
+
+instance (MathApply n (Math a b), MathFnC n) => RealFrac (Math a b) where
+  properFraction a = (unsafeCoerce q, r) where (q, r) = qr a 1
+  truncate         = unsafeCoerce . fn1 Truncate
+  round            = unsafeCoerce . fn1 Round
+  ceiling          = unsafeCoerce . fn1 Ceiling
+  floor            = unsafeCoerce . fn1 Floor
+
+instance (MathApply n (Math a b), MathFnC n) => RealFloat (Math a b) where
+  floatRadix _     = error "floatRadix is undefined for Math"
+  floatDigits _    = error "floatDigits is undefined for Math"
+  floatRange _     = error "floatRange is undefined for Math"
+  decodeFloat _    = error "decodeFloat is undefined for Math"
+  encodeFloat _ _  = error "decodeFloat is undefined for Math"
+  isDenormalized _ = error "isDenormalized is undefined for Math"
+  isNegativeZero _ = error "isNegativeZero is undefined for Math"
+  isIEEE _         = error "isIEEE is undefined for Math"
+  isNaN _          = error "isNaN is undefined for Math"
+  isInfinite _     = error "isInfinite is undefined for Math"
+  atan2            = fn2 Atan2
--- src/Wumber/SymMath.hs
+++ src/Wumber/SymMath.hs
@@ -53,8 +53,10 @@ import Wumber.SymMatch
 -- | Symbolic math expressions. Structurally identical to 'Sym', but typed to
 --   implement Haskell numeric classes. @f@ should be specified to be
 --   convertible from @MathFn@.
-newtype SymMath f a = SM { unSM :: Sym (MathProfile f) f a }
+type SymMath f a = Math (Sym (MathProfile f) f a) SymMathPhantom
+data SymMathPhantom
 
+-- TODO: support real profiles
 type MathProfile f = NoProfiles f
 
 -- | The typeclass context you'll need in order to instantiate 'SymMath'.
@@ -68,11 +70,11 @@ type SymMathC f a = (Fingerprintable a,
 
 
 -- | Promotes a value into a 'SymMath' expression.
-val = SM . sym_val
+val = Math . sym_val
 
 -- | Constructs a 'SymMath' expression that refers to a variable. Variables 0-3
 --   are aliased as 'x_', 'y_', 'z_', and 't_' respectively.
-var = SM . sym_var
+var = Math . sym_var
 
 x_ = var 0          -- ^ An alias for @var 0@
 y_ = var 1          -- ^ An alias for @var 1@
@@ -86,7 +88,7 @@ d_ = val (As 3)     -- ^ An alias for @val (As 3)@
 
 -- | Reduces the symbolic expression to a constant, or 'Nothing' if it has
 --   unknown dependencies.
-val_of = sym_val_of . unSM
+val_of = sym_val_of . unMath
 
 
 -- | A basic 'sym_apply' implementation for math functions.
@@ -99,14 +101,14 @@ math_sym_apply f xs = sym_apply_cons f xs
 
 
 instance (Show a, FnShow f) => Show (SymMath f a) where
-  show (SM (SymC x)) = show x
-  show (SM (SymV i)) | i == 0 = "x_"
-                     | i == 1 = "y_"
-                     | i == 2 = "z_"
-                     | i == 3 = "t_"
-                     | otherwise = "v" ++ show i
+  show (Math (SymC x)) = show x
+  show (Math (SymV i)) | i == 0 = "x_"
+                       | i == 1 = "y_"
+                       | i == 2 = "z_"
+                       | i == 3 = "t_"
+                       | otherwise = "v" ++ show i
 
-  show (SM (SymF f xs _)) = fshow f $ map (show . SM) $ V.toList xs
+  show (Math (SymF f xs _)) = fshow f $ map show $ V.toList xs
 
 instance (Fingerprintable a,
           MathFnC a,
@@ -121,98 +123,12 @@ instance MathFnC a => ValApply MathFn a where
                     ++ " to list of arity " ++ show (length xs)
 
 
-fn1 f a   = SM $ sym_apply (fn f) [unSM a]
-fn2 f a b = SM $ sym_apply (fn f) $ map unSM [a, b]
-
-instance SymMathC f a => Num (SymMath f a) where
-  fromInteger = val . fromInteger
-  (+)         = fn2 Add
-  (*)         = fn2 Mul
-  negate      = fn1 Negate
-  abs         = fn1 Abs
-  signum      = fn1 Signum
-
-instance SymMathC f a => Fractional (SymMath f a) where
-  fromRational = val . fromRational
-  recip        = fn1 Recip
-
-instance SymMathC f a => Floating (SymMath f a) where
-  (**)  = fn2 Pow
-  pi    = val pi
-  exp   = fn1 Exp
-  log   = fn1 Log
-  sin   = fn1 Sin
-  cos   = fn1 Cos
-  tan   = fn1 Tan
-  asin  = fn1 Asin
-  acos  = fn1 Acos
-  atan  = fn1 Atan
-  sinh  = fn1 Sinh
-  cosh  = fn1 Cosh
-  tanh  = fn1 Tanh
-  asinh = fn1 Asinh
-  acosh = fn1 Acosh
-  atanh = fn1 Atanh
-
-instance SymMathC f a => ClosedComparable (SymMath f a) where
-  upper a b = SM $ sym_apply (fn IfNN) [unSM $ a - b, unSM a, unSM b]
-  lower a b = SM $ sym_apply (fn IfNN) [unSM $ a - b, unSM b, unSM a]
-
-
-instance Eq (SymMath f a) where
-  _ == _ = error $ "SymMath instances are not comparable; use IfNN "
-           ++ "(or, for structural comparison, unwrap with unSM)"
-
-instance Ord (SymMath f a) where
-  compare _ _ = error $ "SymMath instances are not comparable; use IfNN "
-                ++ "(or, for structural comparison, unwrap with unSM)"
-
-
--- | A quotient/remainder function we reuse in several places.
---
---   NOTE: this results in 'divMod' and 'quotRem' that behave identically,
---   including for negative divisors. This is technically not the way 'mod' and
---   'rem' should be defined, but it lets us keep things simple for now.
---
---   TODO: is the fix as simple as having 'qr' use 'truncate' and 'divmod' use
---   'floor'?
-
-qr a b = (q, r) where q = truncate (a / b); r = a - q*b
-
-
 instance Integral Double where toInteger = truncate; quotRem = qr
 instance Integral Float  where toInteger = truncate; quotRem = qr
 
 
-instance SymMathC f a => Enum (SymMath f a) where
-  toEnum     = fromInteger . toInteger
-  fromEnum _ = error "can't collapse SymMath to Int via fromEnum"
-  pred x     = x - 1
-  succ x     = x + 1
-
-instance SymMathC f a => Real (SymMath f a) where
-  toRational _ = error "can't collapse SymMath to Rational via toRational"
-
-instance SymMathC f a => Integral (SymMath f a) where
-  toInteger _ = error "can't collapse SymMath to Integer; use Truncate"
-  quotRem a b = (fn2 Quot a b, fn2 Rem a b)
-
-instance SymMathC f a => RealFrac (SymMath f a) where
-  properFraction a = (unsafeCoerce q, r) where (q, r) = qr a 1
-  truncate         = unsafeCoerce . fn1 Truncate
-  round            = unsafeCoerce . fn1 Round
-  ceiling          = unsafeCoerce . fn1 Ceiling
-  floor            = unsafeCoerce . fn1 Floor
-
-instance SymMathC f a => RealFloat (SymMath f a) where
-  floatRadix _     = error "floatRadix is undefined for SymMath"
-  floatDigits _    = error "floatDigits is undefined for SymMath"
-  floatRange _     = error "floatRange is undefined for SymMath"
-  decodeFloat _    = error "decodeFloat is undefined for SymMath"
-  encodeFloat _ _  = error "decodeFloat is undefined for SymMath"
-  isDenormalized _ = error "isDenormalized is undefined for SymMath"
-  isNegativeZero _ = error "isNegativeZero is undefined for SymMath"
-  isIEEE _         = error "isIEEE is undefined for SymMath"
-  isNaN _          = error "isNaN is undefined for SymMath"
-  isInfinite _     = error "isInfinite is undefined for SymMath"
-  atan2            = fn2 Atan2
+instance (MathFnC a, Fingerprintable a) => MathApply a (SymMath MathFn a) where
+  fn0                              = val
+  fn1 f (Math x)                   = Math $ sym_apply f [x]
+  fn2 f (Math x) (Math y)          = Math $ sym_apply f [x, y]
+  fn3 f (Math x) (Math y) (Math z) = Math $ sym_apply f [x, y, z]
