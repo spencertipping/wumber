53cde8c6a5b5250ed2d3497ed421b64e93989dfd
Spencer Tipping <spencer.tipping@gmail.com>
2019-12-13 00:39:51 -0700

    Deleting more obsolete code

--- src/Wumber.hs
+++ src/Wumber.hs
@@ -2,8 +2,6 @@ module Wumber (
   module Wumber.BoundingBox,
   module Wumber.ClosedComparable,
   module Wumber.Constraint,
-  module Wumber.Cursor,
-  module Wumber.Element,
   module Wumber.Fingerprint,
   module Wumber.MathFn,
   module Wumber.Model,
@@ -17,8 +15,6 @@ module Wumber (
 import Wumber.BoundingBox
 import Wumber.ClosedComparable
 import Wumber.Constraint
-import Wumber.Cursor
-import Wumber.Element
 import Wumber.Fingerprint
 import Wumber.MathFn
 import Wumber.Model
